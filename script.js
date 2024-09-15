0a9548553213dfa6183a7eccbf195385da800c41
Abdur Rahman <152638668+abdurrahman2028@users.noreply.github.com>
2024-09-15 20:24:26 +0600

    fast Commit

---
+++ script.js
@@ -0,0 +1,100 @@
+
+
+// let bangladesh = 99;
+// let pakistan = 70;
+// let india = 32;
+
+// let a = bangladesh > india;
+// console.log(a);
+
+// if (bangladesh > india || bangladesh > pakistan & bangladesh == 99) {
+//     console.log("Bangladesh is win");
+// } else {
+//     console.log("gay India");
+// }
+
+
+
+
+// let apple = 80;
+// let banana = 50;
+// let lemon = 130;
+
+// let check = apple > banana;
+// console.log(check);
+
+
+// if (lemon > apple && lemon > banana) {
+//     console.log('lemon is big')
+// } else{
+//     console.log('lemon is small')
+// }
+
+
+
+
+// let value = 80;
+// let grade = '';
+
+// if (value <= 80){
+//     grade = 'A+'
+// } 
+// else if (value <= 70 || value <= 79) {
+//     grade = 'A'
+// }
+// else if (value >= 32) {
+//     grade = 'F'
+// }
+
+// console.log(grade);
+
+
+
+
+
+
+// let abdurRahman = 299;
+// let tanisha = 199;
+
+// if (abdurRahman > tanisha) {
+//     console.log('abdurRahman is king')
+// } 
+// else if (tanisha > abdurRahman) {
+//     console.log('tanisha is Queen')
+// } else {
+//     console.log('Broken')
+// }
+
+
+
+
+let a = 10;
+let b = 20;
+let c = 30;
+let d = 40;
+let e = 50;
+
+let x = a + b + c + d + e;
+
+if (10 == a || 20 == b || 30 == c || 40 == d || 50 == e & a + b + c + d + e == x){
+    console.log('a == a')
+    console.log('b == b')
+    console.log('c == c')
+    console.log('d == d')
+    console.log('e == e')
+} else {
+    console.log('a != a \nb != b \nc != c \nd != d \ne != e')
+}
+
+console.log('sum = ' + x);
+
+
+
+
+
+
+
+
+
+
+
